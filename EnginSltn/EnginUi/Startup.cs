4fef984f6007eb858d47be403ebafe64bf8e4a61
NGrujovic <ngrujovic99@gmail.com>
2023-11-17 12:07:51 +0100

    First Commit/ Created Project

---
+++ EnginSltn/EnginUi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnginUi.Startup))]
+namespace EnginUi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
